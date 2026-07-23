cc2fa8d9884e79f1f52d7dcb39f9e0d0a75cad47
김동준 <wpshs0418@gmail.com>
2026-07-23 14:14:03 +0900

    easy commit

--- problem_solve/3주차/[Easy] [PGS] 올바른 괄호/Nerororo.java
+++ problem_solve/3주차/[Easy] [PGS] 올바른 괄호/Nerororo.java
@@ -1,10 +1,23 @@
-import java.io.*;
-import java.util.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') stack.offer('(');
+            else {
+                if (stack.isEmpty()) {
+                    answer = false;
+                    break;
+                }
+                stack.pollLast();
+            }
+        }
+        if (!stack.isEmpty()) answer = false;
+        return answer;
     }
 }
--- problem_solve/3주차/[Hard] [PGS] 도둑질/Nerororo.java
+++ problem_solve/3주차/[Hard] [PGS] 도둑질/Nerororo.java
@@ -1,10 +1,24 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
-    }
-}
+//import java.io.*;
+//import java.util.*;
+//
+////1부터 len까지 순회하면서 dp 작성, dp = 도달시 각 위치별 최대 숫자(도달 시 작으면 break;)
+////
+////이동 규칙 : +2칸부터 무한 순회
+////for문 돌려서 계속 이동.
+//
+//class Solution {
+//    public int solution(int[] money) {
+//        int len = money.length;
+//        int max = 0;
+//
+//        int[] dp = new int[len];
+//        for (int i = 0; i < len; i++) {
+//
+//        }
+//
+//
+//
+//        int answer = 0;
+//        return answer;
+//    }
+//}
