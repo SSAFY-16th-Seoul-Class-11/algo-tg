cbca91d13a0ddb1def1dd7da720f1a76a77ff823
김동준 <wpshs0418@gmail.com>
2026-07-23 15:52:27 +0900

    normal

--- problem_solve/3주차/[Normal] [PGS] 네트워크/Nerororo.java
+++ problem_solve/3주차/[Normal] [PGS] 네트워크/Nerororo.java
@@ -1,10 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+
+    }
+
+    class Solution {
+        public int solution(int n, int[][] computers) {
+            Queue<Integer> queue = new ArrayDeque<>();
+            boolean[] visited = new boolean[n];
+            int answer = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+
+                    while (!queue.isEmpty()) {
+                        int curr = queue.poll();
+                        for (int j = 0; j < n; j++) {
+                            if (computers[curr][j] == 1 && !visited[j]) {
+                                queue.offer(j);
+                                visited[j] = true;
+                            }
+                        }
+                    }
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
     }
 }
